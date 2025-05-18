b725f8b7fc7937917c175c3532fcb60e33c139ff
Abhay <abhaysharma.cs2017.pgdav@gmail.com>
2025-05-18 11:59:16 +0530

    extracting transcript from youtube video

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from youtube_transcript_api import TranscriptsDisabled, YouTubeTranscriptApi
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel
+from dotenv import load_dotenv
 
+load_dotenv()
+ytt_api = YouTubeTranscriptApi()
 st.set_page_config(page_title= 'YouTube Chatbot')
 st.title('Youtube Chatbot App')
 st.sidebar.header("YouTube Video")
@@ -10,12 +19,17 @@ with st.chat_message("assistant"):
 
 
 
-# if video_id:
-#     active_state = True
+if video_id:
+    active_state = True
+    raw_transcript = ytt_api.get_transcript(video_id, languages= ['en', 'hi'])
+    transcript = " ".join(chunk['text'] for chunk in raw_transcript)
+
 
 query = st.chat_input("Ask your question", disabled= not bool(video_id))
 if query:
-    st.write(f'User has sent the following query {query}')
+    #st.write(f'User has sent the following query {query}')
+    with st.chat_message("assistant"):
+        st.write(transcript)
 else:
 # Disable chat input
     st.info("Please enter a YouTube video ID in the sidebar to start chatting.", icon = "🚨")
